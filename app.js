52fdaa9863e6effd93dafe2cb49de0793dad0e21
robin <rwilson@troolr.com>
2019-11-02 18:15:18 -0700

    added glitch files and cleaned up code

---
+++ app.js
@@ -0,0 +1,58 @@
+// app.js
+//
+// Uses express and google translate to make translate API requests
+
+const express = require("express");
+const app = express();
+
+// Google translate
+const { Translate } = require('@google-cloud/translate').v2;
+const util = require("util");
+
+const secrets = require("./secrets.json");
+
+let translateCache = {};
+let translate = new Translate({
+    projectId: secrets.PROJECT_ID,
+    key: secrets.GOOGLE_API_KEY
+});
+
+app.use(express.static("public"));
+
+// Translate the word passed in using Google Translate
+app.get("/translate/:language/:englishword", function (request, response) {
+
+    sendToTranslateAPIIfNotInCache(request.params.englishword, request.params.language)
+        .then(translation => {
+            response.send(translation)
+        })
+        .catch(e => { console.log(e, "error") })
+});
+
+const listener = app.listen(secrets.PORT, function () {
+    console.log("Your app is listening on port " + listener.address().port);
+});
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/views/index.html");
+});
+
+// Send translation to Google Translate
+// englishWord
+// targetLanguage // ex - "zh-CN";
+async function sendToTranslateAPIIfNotInCache(englishWord, targetLanguage) {
+    // Check to see if english word was already translated
+    if (translateCache[englishWord] && translateCache[englishWord][targetLanguage]) {
+        // if already translated, send the translation immediately
+        return translateCache[englishWord][targetLanguage];
+    } else {
+        // if not translated, get translation from google
+        const [translation] = await translate.translate(englishWord, targetLanguage);
+        // put translation into translateCache
+        if (!translateCache[englishWord]) translateCache[englishWord] = {};
+        translateCache[englishWord][targetLanguage] = translation
+        return translation;
+    }
+    // something went wrong
+    return null;
+}
---
+++ public/js/client.js
@@ -0,0 +1,144 @@
+// components.js
+//
+// Front-end helper functions for API calls, registering A-Frame components and generating language dropdowns.
+
+// constant variables
+const LANGUAGES = {
+	english: {
+		dropDownText: "English",
+		acronym: "en"
+	},
+	chinese: {
+		// simplified Chinese
+		dropDownText: "Chinese (simplified)",
+		acronym: "zh-CN"
+	},
+	french: {
+		dropDownText: "French",
+		acronym: "fr"
+	},
+	spanish: {
+		dropDownText: "Spanish",
+		acronym: "es"
+	},
+	italian: {
+		dropDownText: "Italian",
+		acronym: "it"
+	},
+	german: {
+		dropDownText: "German",
+		acronym: "de"
+	},
+	japanese: {
+		dropDownText: "Japanese",
+		acronym: "ja"
+	}
+};
+
+// dynamic variables w/ default values
+let startLanguage = "english";
+let targetLanguage = "french";
+
+AFRAME.registerComponent("translatable", {
+	init: function () {
+		const el = this.el;
+		el.addEventListener("click", ev => {
+			const englishWord = el.getAttribute("englishword");
+			// if element has a specific placement for the translation, make sure to add that to the attributes and add that to the inner html
+
+			// potential race condition between the two languages
+			// TODO make DRY, add Promise.all
+			if (startLanguage === "english") {
+				// already translated, simply set the english word
+				let startLanguageTextElem = document.getElementById("startLanguageText");
+				startLanguageTextElem.innerHTML = englishWord;
+				setRemoveTranslatedWordsInterval();
+			} else {
+				$.get(
+					`/translate/${LANGUAGES[startLanguage].acronym}/${englishWord}`,
+					function (data, status) {
+						let startLanguageTextElem = document.getElementById("startLanguageText");
+						startLanguageTextElem.innerHTML = data;
+						setRemoveTranslatedWordsInterval();
+					}
+				);
+			}
+
+			if (targetLanguage === "english") {
+				// already translated, simply set the english word
+				let translateTextElem = document.getElementById("translatedText");
+				translateTextElem.innerHTML = englishWord;
+				setRemoveTranslatedWordsInterval();
+			} else {
+				$.get(
+					`/translate/${LANGUAGES[targetLanguage].acronym}/${englishWord}`,
+					function (data, status) {
+						let translateTextElem = document.getElementById("translatedText");
+						translateTextElem.innerHTML = data;
+						setRemoveTranslatedWordsInterval();
+					}
+				);
+			}
+		});
+	}
+});
+
+
+let removeTranslatedWordsInterval = null; // used with removing the translated text from the screen
+
+// resets timeout for removing the translated words from the screen back to 3 seconds
+function setRemoveTranslatedWordsInterval() {
+	if (removeTranslatedWordsInterval) {
+		clearTimeout(removeTranslatedWordsInterval);
+	}
+	removeTranslatedWordsInterval = setTimeout(
+		removeTranslatedWordsFromScreen,
+		3000
+	);
+}
+
+// removes the translated words from the screen
+function removeTranslatedWordsFromScreen() {
+	let translateTextElem = document.getElementById("translatedText");
+	let startLanguageTextElem = document.getElementById("startLanguageText");
+	translateTextElem.innerHTML = "";
+	startLanguageTextElem.innerHTML = "";
+
+	removeTranslatedWordsInterval = null;
+}
+
+// creates options for the language dropdowns using the LANGUAGES object
+function createLanguageSelectionDropdowns() {
+	var dropDownStartLanguage = document.getElementById("startLanguage");
+	var dropDownTargetLanguage = document.getElementById("targetLanguage");
+
+	var languageList = Object.keys(LANGUAGES);
+	var options = "";
+	for (var i = 0; i < languageList.length; i++) {
+		let currentLanguage = languageList[i];
+		options += `<option value="${currentLanguage}">${LANGUAGES[currentLanguage].dropDownText}</option>`;
+	}
+
+	dropDownStartLanguage.innerHTML = options;
+	dropDownTargetLanguage.innerHTML = options;
+
+	// set defaults
+	dropDownStartLanguage.value = startLanguage;
+	dropDownTargetLanguage.value = targetLanguage;
+
+	// set onchange event to change the start and target LANGUAGES
+	dropDownStartLanguage.onchange = function () {
+		startLanguage = this.value;
+	};
+	dropDownTargetLanguage.onchange = function () {
+		targetLanguage = this.value;
+	};
+}
+
+function init() {
+	document.addEventListener("DOMContentLoaded", () => {
+		createLanguageSelectionDropdowns();
+	});
+}
+
+init();
